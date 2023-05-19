254e089a8e3a3ace411390586ab81cccaebdf511
Fedor Romanovschi <110813692+lordferro@users.noreply.github.com>
2023-05-19 09:16:40 +0200

    Add Filtering

--- src/commonStyles.styled.js
+++ src/commonStyles.styled.js
@@ -1,6 +1,20 @@
 import { NavLink } from 'react-router-dom';
 import styled from 'styled-components';
 
+export const Container = styled.div`
+display:flex;
+flex-direction: column;
+align-items: center;
+`
+
+export const StyledLabel = styled.label`
+  font-weight: 600;
+  font-size: 18px;
+  line-height: 22px;
+  color: #373737;
+  margin-right: 5px;
+`;
+
 export const Button = styled.button`
   cursor: pointer;
   font-weight: 600;
@@ -14,8 +28,8 @@ export const Button = styled.button`
   box-shadow: 0px 3.43693px 3.43693px rgba(0, 0, 0, 0.25);
   margin-top: 26px;
   display: block;
-  margin-left: auto;
-  margin-right: auto;
+  /* margin-left: auto;
+  margin-right: auto; */
   background: #ebd8ff;
 
   &.following {
@@ -39,7 +53,7 @@ export const StyledLink = styled(NavLink)`
   height: 50px;
   border-radius: 10.3108px;
   box-shadow: 0px 3.43693px 3.43693px rgba(0, 0, 0, 0.25);
-  margin-top: 26px;
+  margin-bottom: 26px;
   display: block;
   margin-left: auto;
   margin-right: auto;
--- src/pages/TweetsPage.jsx
+++ src/pages/TweetsPage.jsx
@@ -1,30 +1,72 @@
 import { User } from 'components/User/User';
-import { Button } from 'commonStyles.styled';
-import { useState } from 'react';
+import { Button, Container, StyledLabel, StyledLink } from 'commonStyles.styled';
+import { useEffect, useState } from 'react';
 import { useGetUsersQuery } from 'redux/tweetsApi';
+
 
 const Tweets = () => {
   const [page, setPage] = useState(3);
   const { data: users, error, isLoading } = useGetUsersQuery(page);
+  const [filter, setFilter] = useState('');
+  const [usersToShow, setUsersToShow] = useState(users);
 
+  useEffect(() => {
+    const filtering = () => {
+      switch (filter) {
+        case 'following':
+          setUsersToShow(users.filter(user => user.following === true));
+          break;
+        case 'not following':
+          setUsersToShow(users.filter(user => user.following === false));
+          break;
+        default:
+          setUsersToShow(users);
+          break;
+      }
+    };
+    filtering();
+  }, [filter, users]);
+
+  const handleChange = e => {
+    const value = e.target.value;
+    setFilter(value);
+  };
+  console.log(usersToShow);
   if (isLoading) {
-    return <Button>loading</Button>;
+    return <StyledLink>loading</StyledLink>;
   }
 
   return (
-    <>
+    <Container>
       {error ? (
         <p>Something went wrong</p>
       ) : (
-        <ul>
-          {users.map(user => {
-            return (
-              <li key={user.id}>
-                <User data={user} />
-              </li>
-            );
-          })}
-        </ul>
+        <>
+          <>
+            <StyledLabel>
+              Filter: {' '}
+              <input onChange={handleChange} list="filter" />
+            </StyledLabel>
+            <datalist id="filter">
+              <option value="all" />
+              <option value="following" />
+              <option value="not following" />
+            </datalist>
+          </>
+          {usersToShow && usersToShow.length > 0 ? (
+            <ul>
+              {usersToShow.map(user => {
+                return (
+                  <li key={user.id}>
+                    <User data={user} />
+                  </li>
+                );
+              })}
+            </ul>
+          ) : (
+            <p>no users to show</p>
+          )}
+        </>
       )}
       <Button
         type="button"
@@ -34,7 +76,7 @@ const Tweets = () => {
       >
         Load more
       </Button>
-    </>
+    </Container>
   );
 };
-export default Tweets
+export default Tweets;
